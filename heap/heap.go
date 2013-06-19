88b352f13bc696e157d6807801167b1f56c94c1d
Matthew Sanders <mattomatic@gmail.com>
2013-06-19 09:46:33 -0500

    Added min/max heap functionality.

--- heap/heap.go
+++ heap/heap.go
@@ -6,16 +6,22 @@ type Item interface {
 
 type Heap struct {
 	data []Item
+	min  bool
 }
 
-// Create a new heap
-func New() *Heap {
-    return &Heap{make([]Item, 0)}
+// Create a new min heap
+func NewMinHeap() *Heap {
+	return &Heap{make([]Item, 0), true}
+}
+
+// Create a new max heap
+func NewMaxHeap() *Heap {
+	return &Heap{make([]Item, 0), false}
 }
 
 // Get the number of elements in the heap
 func (h *Heap) Size() int {
-    return len(h.data)
+	return len(h.data)
 }
 
 // Insert an element into the heap
@@ -25,81 +31,100 @@ func (h *Heap) Insert(x Item) {
 }
 
 // Get the smallest element in the heap
-func (h *Heap) Min() Item {
-    return h.data[0]
+func (h *Heap) Peek() Item {
+	return h.data[0]
 }
 
 // Remove the smallest element from the heap
-func (h *Heap) Remove() Item {
-    x := h.data[0]
-    h.swap(0, h.Size() - 1)
-    h.trim()
-    h.siftDown(0)
-    return x
+func (h *Heap) Pop() Item {
+	x := h.data[0]
+	h.swap(0, h.Size()-1)
+	h.trim()
+	h.siftDown(0)
+	return x
 }
 
 func (h *Heap) siftUp(c int) {
-    p := parent(c)
-    
-    if !h.invariantHolds(p, c) {
-        h.swap(p, c)
-        h.siftUp(p)
-    }
+	p := parent(c)
+
+	if !h.invariantHolds(p, c) {
+		h.swap(p, c)
+		h.siftUp(p)
+	}
 }
 
 func (h *Heap) siftDown(p int) {
-    c1 := lhsChild(p)
-    c2 := rhsChild(p)
-    
-    if !(h.invariantHolds(p, c1) && h.invariantHolds(p, c2)) {
-        if c2 >= h.Size() || h.data[c1].Less(h.data[c2]) {
-            h.swap(p, c1)
-            h.siftDown(c1)
-        } else {
-            h.swap(p, c2)
-            h.siftDown(c2)
-        }
-    }   
+	c1 := lhsChild(p)
+	c2 := rhsChild(p)
+
+	if !(h.invariantHolds(p, c1) && h.invariantHolds(p, c2)) {
+		if c2 >= h.Size() || h.less(c1, c2) {
+			h.swap(p, c1)
+			h.siftDown(c1)
+		} else {
+			h.swap(p, c2)
+			h.siftDown(c2)
+		}
+	}
 }
 
 func (h *Heap) invariantHolds(p int, c int) bool {
-    // all children must be greater than or equal to their parents
-    // <==>
-    // all children must not be less than their parents
-    // note: if there are no children, then the invariant holds
-    return c >= h.Size() || !h.data[c].Less(h.data[p])
-}  
+	// all children must be greater than or equal to their parents
+	// <==>
+	// all children must not be less than their parents
+	// note: if there are no children, then the invariant holds
+	return c >= h.Size() || !h.less(c, p)
+}
 
 func parent(x int) int {
-    if x == 0 {
-        return 0
-    }
-    
-    return halve(x - 1)
+	if x == 0 {
+		return 0
+	}
+
+	return halve(x - 1)
 }
 
 func lhsChild(x int) int {
-    return double(x) + 1
+	return double(x) + 1
 }
 
 func rhsChild(x int) int {
-    return double(x) + 2
+	return double(x) + 2
 }
 
 func double(x int) int {
-    return x << 1
+	return x << 1
 }
 
 func halve(x int) int {
-    return x >> 1
+	return x >> 1
 }
 
 func (h *Heap) swap(x int, y int) {
-    t := h.data[x]
-    h.data[x] = h.data[y]
-    h.data[y] = t
+	t := h.data[x]
+	h.data[x] = h.data[y]
+	h.data[y] = t
+}
+
+func (h *Heap) less(x int, y int) bool {
+	a := h.data[x]
+	b := h.data[y]
+
+	if h.min {
+		return a.Less(b)
+	} else {
+		return greaterThan(a, b)
+	}
+}
+
+func greaterThan(x Item, y Item) bool {
+	return !x.Less(y) && !equalTo(x, y)
+}
+
+func equalTo(x Item, y Item) bool {
+	return !x.Less(y) && !y.Less(x)
 }
 
 func (h *Heap) trim() {
-    h.data = h.data[:len(h.data) - 1]
+	h.data = h.data[:len(h.data)-1]
 }
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -1,97 +1,119 @@
 package heap
 
 import (
-	"testing"
 	"math/rand"
+	"testing"
 )
 
 func TestCreate(t *testing.T) {
-	h := New()
+	h := NewMinHeap()
 
 	if h == nil {
 		t.Error()
 	}
+
+	h2 := NewMaxHeap()
+
+	if h2 == nil {
+		t.Error()
+	}
 }
 
-func TestInvariant(t *testing.T) {
-    a := Int(1)
-    b := Int(1)
-    
-    if a.Less(b) {
-        t.Error()
-    }
-    
-    c := Int(2)
-    
-    if c.Less(a) {
-        t.Error()
-    }
+func TestLess(t *testing.T) {
+	a := Int(1)
+	b := Int(1)
+
+	if a.Less(b) {
+		t.Error()
+	}
+
+	c := Int(2)
+
+	if c.Less(a) {
+		t.Error()
+	}
 }
 
 func TestInsert(t *testing.T) {
-	h := New()
+	h := NewMinHeap()
 	h.Insert(Int(2))
 	h.Insert(Int(3))
 	h.Insert(Int(1))
 	h.Insert(Int(4))
-	
-	if h.Min() != Int(1) {
-	    t.Error()
-	}	
+
+	if h.Peek() != Int(1) {
+		t.Error()
+	}
 }
 
 func TestRemove(t *testing.T) {
-	h := New()
+	h := NewMinHeap()
 	h.Insert(Int(2))
 	h.Insert(Int(3))
 	h.Insert(Int(1))
 	h.Insert(Int(4))
-	h.Remove()
-	h.Remove()
-	h.Remove()
-	h.Remove()
+	h.Pop()
+	h.Pop()
+	h.Pop()
+	h.Pop()
 }
 
 func TestRandom(t *testing.T) {
-    h := New()
-    for i:=0; i<500; i++ {
-        x := Int(rand.Int())
-        h.Insert(x)
-    }
-    
-    x := h.Min()
-    for h.Size() > 0 {
-        if h.Min().Less(x) {
-            t.Error()
-        }
-        
-        x = h.Remove()
-    }
+	h := NewMinHeap()
+	for i := 0; i < 500; i++ {
+		x := Int(rand.Int())
+		h.Insert(x)
+	}
+
+	x := h.Peek()
+	for h.Size() > 0 {
+		if h.Peek().Less(x) {
+			t.Error()
+		}
+
+		x = h.Pop()
+	}
 }
- 
+
+func TestRandomMax(t *testing.T) {
+	h := NewMaxHeap()
+	for i := 0; i < 500; i++ {
+		h.Insert(Int(rand.Intn(100)))
+	}
+
+	x := h.Peek()
+	for h.Size() > 0 {
+		if greaterThan(h.Peek(), x) {
+			t.Error()
+		}
+
+		x = h.Pop()
+	}
+}
+
 func TestParentChild(t *testing.T) {
-    for i := 0; i < 50; i++ {
-        c1 := lhsChild(i)
-        c2 := rhsChild(i)
-        
-        if c1 == c2 {
-            t.Error()
-        }
-        
-        if parent(c1) != i || parent(c2) != i {
-            t.Error()
-        }
-    }
+	for i := 0; i < 50; i++ {
+		c1 := lhsChild(i)
+		c2 := rhsChild(i)
+
+		if c1 == c2 {
+			t.Error()
+		}
+
+		if parent(c1) != i || parent(c2) != i {
+			t.Error()
+		}
+	}
 }
 
 func BenchmarkDouble(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        double(i)
-    }
+	for i := 0; i < b.N; i++ {
+		double(i)
+	}
 }
 
 func BenchmarkHalve(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        halve(i)
-    }
+	for i := 0; i < b.N; i++ {
+		halve(i)
+	}
 }
